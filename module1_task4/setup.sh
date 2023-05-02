8aaec375ffb43245a4016a170b8984c11be8e430
EXTRADEEN <dinumira@gmail.com>
2023-05-02 15:44:40 +0200

    fixed production deployment

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,4 +1,16 @@
-docker run --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 ./setup.sh
+#!/bin/bash
+
+# Install required packages
 apt-get update && apt-get install -y hugo make
+
+# Install 'curl'
+apt-get install curl -y
+
+# Download release of 'Hugo'
+curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
+
+# Install version of 'Hugo'
+apt-get install ./install_hugo.deb
+
+# Build the website
 make build
-exit $?
