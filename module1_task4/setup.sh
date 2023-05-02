3ff56665f4dfec5342761d4f8e1d6d1c71c6e58a
EXTRADEEN <dinumira@gmail.com>
2023-05-02 15:50:33 +0200

    fixed production deployment

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -7,7 +7,7 @@ apt-get update && apt-get install -y hugo make
 apt-get install curl -y
 
 # Download release of 'Hugo'
-curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
+curl -Lo install_hugo.deb https://github.com/gohugoio/hugo/releases/download/v0.84+/hugo_extended_0.84+_linux-amd64.deb
 
 # Install version of 'Hugo'
 apt-get install ./install_hugo.deb
