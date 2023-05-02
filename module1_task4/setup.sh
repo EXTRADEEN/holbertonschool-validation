d96a4ea5ee530de377418d72ba7106071189987a
EXTRADEEN <dinumira@gmail.com>
2023-05-02 15:39:07 +0200

    fixed code

--- module1_task4/setup.sh
+++ module1_task4/setup.sh
@@ -1,4 +1,4 @@
-docker run --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 /bin/bash
+docker run --rm --tty --interactive --volume=$(pwd):/app --workdir=/app ubuntu:18.04 ./setup.sh
 apt-get update && apt-get install -y hugo make
 make build
 exit $?
