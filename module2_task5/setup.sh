b9ab364e67c3ffb47be05b88d60304169f1cf931
EXTRADEEN <dinumira@gmail.com>
2023-05-11 12:02:46 +0200

    added instalation

--- module2_task5/setup.sh
+++ module2_task5/setup.sh
@@ -6,4 +6,10 @@ wget https://github.com/gohugoio/hugo/releases/download/v0.84.0/hugo_extended_0.
 tar -zxvf hugo_extended_0.84.0_Linux-64bit.tar.gz
 mv hugo /usr/local/bin
 
+# Install markdown-cli v0.26.0
+npm install -g markdown-cli@0.26.0
+
+# Install markdown-link-check v3.8.6
+npm install -g markdown-link-check@3.8.6
+
 make build
